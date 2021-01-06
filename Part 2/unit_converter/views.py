3789dd0979525e81cdd1a9ab24f5b80a8cbe9544
Jean <jeanmobayed@gmail.com>
2021-01-06 06:02:07 -0400

    Removing the logger

--- Part 2/unit_converter/views.py
+++ Part 2/unit_converter/views.py
@@ -7,12 +7,9 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 
-import logging
 import json
 
 from unit_converter.modules.input_parser import InputParser
-
-logger = logging.getLogger(__name__)
 
 def home(request):
 	return render(request, 'unit_converter/home.html')
@@ -25,15 +22,11 @@ def process_input(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
     input_array = body["input"].splitlines() 
-    #logger.error(input_array)
 
     parser = InputParser(DEFAULT_ANSWER)
 
     info = parser.clasify_input(input_array)
     error_msgs = info['error_msgs']
-
-    logger.error(info)
-
 
     if len(info['ref_words']) > 0:
         result = parser.process_info(info['ref_words'], info['price_msgs'])
@@ -47,9 +40,6 @@ def process_input(request):
 
         prices = parser.get_price_book()
         word = parser.get_words_book()
-
-        logger.error(word)
-        logger.error(prices)
 
         return JsonResponse(answers, safe=False)
 
